44e32bc9b46ae2631d7b675d395be7f0162ec7b9
AbdulAzeez <azeez032001@gmail.com>
2026-07-15 14:28:34 +0530

    Review changes updated

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -1,12 +1,14 @@
 import "../css/GroupCard.css";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import GroupChat from "../pages/Groups/GroupChat";
+// import GroupRequestDialog from "../pages/groups/GroupRequestDialog";
 export default function GroupCard({group}){
  
 const navigate=useNavigate();
- 
+//  const [open,setOpen]=useState(false);
 return(
- 
+ <>
 <div className="group-card">
  
 <div className="group-image">
@@ -15,7 +17,6 @@ return(
 src={group.groupimage}
 alt={group.groupname}
 />
- 
 </div>
  
 <div className="group-content">
@@ -43,13 +44,26 @@ navigate(`/group/details/${group._id}`);
 >
 Add Member
 </button>
+{/* <button
+onClick={(e)=>{
+e.stopPropagation();
+setOpen(true);
+}}
+>
+View Requests
+</button> */}
 </div>
 </div>
  
 </div>
  
 </div>
- 
+  {/* <GroupRequestDialog
+      open={open}
+      onClose={() => setOpen(false)}
+      groupid={group?._id}
+    /> */}
+    </>
 );
  
 }
--- src/hooks/group/useGroupDetails.jsx
+++ src/hooks/group/useGroupDetails.jsx
@@ -2,23 +2,26 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { URL } from "../../../config";
 
-const getGroupDetails=async(groupid)=>{
-  const response=await axios.get(
+const getGroupDetails = async (groupid) => {
+
+  const response = await axios.get(
     `${URL}/group/details/${groupid}`,
-    {},
-{
-headers:{
-Authorization:`Bearer ${localStorage.getItem("token")}`
-}
-}
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
   );
+
   return response.data;
 };
 
-export const useGroupDetails=(groupid)=>{
+export const useGroupDetails = (groupid) => {
+
   return useQuery({
-    queryKey:["group-details",groupid],
-    queryFn:()=>getGroupDetails(groupid),
-    enabled:!!groupid
+    queryKey: ["group-details", groupid],
+    queryFn: () => getGroupDetails(groupid),
+    enabled: !!groupid,
   });
+
 };
